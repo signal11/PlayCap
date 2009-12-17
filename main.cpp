2cdd147706fbeb816346bc0b58a5859099581297
Alan Ott <alan@ato.(none)>
2009-12-17 12:10:49 -0500

    Entered the GPL header on each file.

--- main.cpp
+++ main.cpp
@@ -1,8 +1,21 @@
-/*******************************
- FOX GUI for AcuScene Scanner
- Alan Ott. AcuSoft, Inc.
- 5-6-2003
-*******************************/
+/**************************************************************************
+    Main GUI Entry
+    Copyright 2009 Alan Ott, Signal 11 Software
+    
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+***************************************************************************/
 
 
 #include <fx.h>
